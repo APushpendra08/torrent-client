fa07517375a4e200679f3ea6e0cb586584e774a7
A Pushpendra <aladupushpendra08@gmail.com>
2024-03-22 14:38:09 +0530

    added the files

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ import fs from 'fs';
 import bencode from "bencode"
 
 const torrent = bencode.decode(fs.readFileSync('./puppy.torrent'))
-console.log(torrent.announce.toString('utf8'))
+// console.log(String.fromCharCode.apply(null, torrent.announce));
+console.log(Buffer.from(torrent.announce).toString());
